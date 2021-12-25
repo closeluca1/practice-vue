7f55a66e7fccf9a457790b23422242462241bb3d
Patrick Lucas <contatopatricklucas@gmail.com>
2021-12-25 04:15:03 -0300

    trocar imagem usando

---
+++ src/components/changeImg.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <button @click="$emit('changeImg')">trocar imagem do perfil</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'changeImg',
+        emits: ['changeImg']
+    }
+</script>
--- src/components/picture.vue
+++ src/components/picture.vue
@@ -1,15 +1,33 @@
 <template>
     <img :src="avatar" :alt="description">
+    <changeImg @changeImg="imgChange"/>
 </template>
 
 <script>
+    import changeImg from './changeImg.vue';
     export default {
         name: 'pictureA',
+        components: {
+            changeImg
+        },
         data() {
             return {
                 avatar: 'https://conteudo.imguol.com.br/c/entretenimento/85/2021/07/16/alpaca-1626455789996_v2_4x3.jpg',
                 description: 'ícone do Vue'
             }
-        }   
+        },
+        methods: {
+            imgChange() {
+                console.log('imagem trocada');
+                this.avatar = 'https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg'
+            }
+        }
     }
 </script>
+
+<style global>
+img{
+    width: 320px;
+}
+
+</style>
